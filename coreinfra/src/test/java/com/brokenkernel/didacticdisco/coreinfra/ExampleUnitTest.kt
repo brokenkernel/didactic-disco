7a49ff88dcc7e3ff3db66f3e2bebc82d1e97c6b6
Eitan Adler <lists@eitanadler.com>
2026-07-03 15:37:52 -0700

    [init] clean up autogen docs for tests

--- coreinfra/src/test/java/com/brokenkernel/didacticdisco/coreinfra/ExampleUnitTest.kt
+++ coreinfra/src/test/java/com/brokenkernel/didacticdisco/coreinfra/ExampleUnitTest.kt
@@ -3,12 +3,7 @@ package com.brokenkernel.didacticdisco.coreinfra
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+internal class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
